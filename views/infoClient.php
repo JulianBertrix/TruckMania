56fb5a36bf73a5bcf36a067d341ddb2ea83f1fc5
Julian Bertrix <julianbertrix@beweb-16.176Q>
2018-06-25 18:00:05 +0200

    template et test de la page de profil d'un client

---
+++ views/infoClient.php
@@ -0,0 +1,17 @@
+<div class="container bootstrap snippet">
+            <div class="row">
+                        <div class="col-sm-10"><h1>Nom du client</h1></div>
+                <div class="col-sm-2"><a href="/users" class="pull-right"><img title="profile image" class="img-circle img-responsive" src=""></a></div>
+            </div>
+            <div class="row">
+                        <div class="col-sm-3"><!--left col-->
+
+                  <ul class="list-group">
+                    <li class="list-group-item text-muted">mes infos</li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong>nom</strong></span> John</li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong>prenom</strong></span> Doe</li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong>email</strong></span> adresse@email.com</li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong>mot de passe</strong></span> *****</li>
+                    <button>edit</button>
+                  </ul> 
+
