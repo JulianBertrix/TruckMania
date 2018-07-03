70c7dd25860aeee2677d466b21b8c8f1445c55f5
Tom <tcantinelli@gmail.com>
2018-07-03 07:05:44 +0200

    DAOMap OK

--- dao/DAOMap.php
+++ dao/DAOMap.php
@@ -11,24 +11,58 @@ class DAOMap extends DAO{
 
     /**
      * Methode qui prend en argument une adresse et retourne ses coordonnées GPS sous la forme
-     * ["latitude" => xxxxx , "longitude" => xxxxxx]
+     * ["lat" => xxxxx , "lng" => xxxxxx]
      */
 
     public function giveMeTheGPS($adresse) {
+
+        $coord;
 
         $newAdresse = str_replace(" ","+",$adresse);
 
         if ($stream = fopen('https://maps.googleapis.com/maps/api/geocode/json?address='.$newAdresse.'&key=AIzaSyDd0z6MCPdZ0v5TPvkbB6yWW9dli2vkN3c', 'r')) {
             
-            $listeIDsJson = stream_get_contents($stream, -1, 0);
+            $resultJson = stream_get_contents($stream, -1, 0);
     
             //Conversion en tableau
-            $listeIDs = json_decode($listeIDsJson, true);
+            $result = json_decode($resultJson, true);
 
-            var_dump($listeIDs);
-    
             fclose($stream);
+
+            $coord = $result["results"][0]["geometry"]["location"];
         }
+
+        return $coord;
+    }
+
+    /**
+     * Methode qui prend en argument des coordonnées GPS et retourne l'adresse correspondante sous la forme
+     * d'une string
+     */
+    public function giveMeTheAdresse($lat,$lon) {
+
+        $coord = $lat.",".$lon;
+
+        $adresse = "";
+
+        if ($stream = fopen('https://maps.googleapis.com/maps/api/geocode/json?latlng='.$coord.'&key=AIzaSyDd0z6MCPdZ0v5TPvkbB6yWW9dli2vkN3c', 'r')) {
+            
+            $resultJson = stream_get_contents($stream, -1, 0);
+
+    
+            //Conversion en tableau
+            $result = json_decode($resultJson, true);
+
+            fclose($stream);
+
+            $adresse = $result["results"][0]["formatted_address"];
+
+            //Supression du ", France" de l'adresse
+            $adresse = str_replace(", France","",$adresse);
+
+        }
+
+        return $adresse;
     }
 
     public function create($adresse) {
