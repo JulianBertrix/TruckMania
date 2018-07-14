8dcf0aa36cd81f980dffea6509482f34f54be319
Tom <tcantinelli@gmail.com>
2018-07-14 11:46:13 +0200

    Debut profil Client

--- views/infoClient.php
+++ views/infoClient.php
@@ -5,7 +5,7 @@
   <li class="list-group-item text-right mail"><span class="pull-left"><strong>Email<br></strong></span><?php echo $infoClient->getEmail(); ?></li>
   <li class="list-group-item text-right"><span class="pull-left"><strong>Adresse</strong></span><?php echo $infoClient->getAdresseId()->getAdresse(); ?></li>
   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-    Edit
+  <i class="fas fa-user-edit"></i> Editer
   </button>
 </ul>
 <div>
